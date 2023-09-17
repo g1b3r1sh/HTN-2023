3191fc0fc063c0ba83b51253b10b62cc8ca19948
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2023-09-17 01:38:05 -0400

    Fix Cohere functions

--- llm_api.py
+++ llm_api.py
@@ -8,7 +8,7 @@ def gen_key():
 
 def generate_prompt(examples, topic, top_text):
     example_string = "\n".join("Top Text: " + example[0] + "\nBottom Text: " + example[1] for example in examples)
-    prompt = 'Generate a viral image macro advertising the product \"{topic}\" in a similar format to the examples.\n\n{example_string}\nTop Text: {top_text}\nBottom Text: '.format(
+    prompt = 'Generate a viral image macro advertising the product \"{topic}\" in a similar format to the examples.\n\n\n{example_string}\nTop Text: {top_text}\nBottom Text: '.format(
         topic=topic,
         example_string=example_string,
         top_text=top_text
@@ -16,7 +16,7 @@ def generate_prompt(examples, topic, top_text):
     return prompt
 
 # Examples is an array of tuples in format [(top text, bottom text)]
-def generate_text(examples, topic, top_text):
+def generate_text(examples, topic, top_text, num_gens=1):
     co = cohere.Client(gen_key())
     response = co.generate(
         model='command',
@@ -25,12 +25,13 @@ def generate_text(examples, topic, top_text):
         temperature=2,
         k=0,
         stop_sequences=[],
-        return_likelihoods='NONE'
+        return_likelihoods='NONE',
+        num_generations=num_gens
     )
-    return response.generations[0].text
+    return [gen.text for gen in response.generations]
 
 def main():
-    generate_text([('PUSH IN THE SIDES OF YOUR TIN FOIL PACKAGE', 'TWILL KEEP THE BOLL FROM FALLING OUT'), ('THE NIGHT BEFORE, PLACE THINGS YOU DON\'T WANT TO FORGET THE NEXT MORNING...', 'CN TOP OF YOUR SHOES')], 'Lays Chips', 'Eat Lays Chips everyday...')
+    print(generate_text([('PUSH IN THE SIDES OF YOUR TIN FOIL PACKAGE', 'TWILL KEEP THE BOLL FROM FALLING OUT'), ('THE NIGHT BEFORE, PLACE THINGS YOU DON\'T WANT TO FORGET THE NEXT MORNING...', 'CN TOP OF YOUR SHOES')], 'Lays Chips', 'Eat Lays Chips everyday...'))
 
 if __name__ == '__main__':
     main()
